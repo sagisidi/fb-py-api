d5a66b2548faeac239650de522d07095d52925ec
sagi sidi <sagisidi@gmail.com>
2022-05-24 13:49:53 +0300

    create new project and add tests for wait for db command

--- api/api/calc.py
+++ api/api/calc.py
@@ -1,6 +1,9 @@
+
 def add(x, y):
     """Add 2 numbers toghether"""
     return x+y
+
+
 def subtruct(x, y):
     """subtruct 2 numbers"""
     return x-y
--- api/api/test.py
+++ api/api/test.py
@@ -7,6 +7,7 @@ class CalcTest(SimpleTestCase):
     def test_add_numbers(self):
         """Test 2 numbers adding toghether"""
         self.assertEqual(calc.add(3, 8), 11)
+
     def test_sub_numbers(self):
         """Test subtruct 2 number"""
         self.assertEqual(calc.subtruct(8, 3), 5)
